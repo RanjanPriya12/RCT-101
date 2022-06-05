a8d311ff919bb565000f47e30e1b3e6743a944e3
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2022-06-05 23:24:30 +0530

    Form

---
+++ Form/form/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Table from "./Components/Table";
+//import Form from "./Components/Form";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Form/> */}
+      <Table/>
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
